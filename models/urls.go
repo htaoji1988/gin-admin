4c3df5b3f50f103b0b5232bc1955145696c291b8
haitao.ji <htaoji1988@126.com>
2020-03-20 14:03:04 +0800

    gorm 外键问题

--- models/urls.go
+++ models/urls.go
@@ -2,10 +2,6 @@ package models
 
 type Urls struct {
 	ID   uint   `gorm:"primary_key"`
-	Name string `gorm:"type:varchar(30);"`
+	Name string `gorm:"type:varchar(32);"`
 	Url  string `gorm:"type:varchar(255);"`
-}
-
-func (Urls) TableName() string {
-	return "usermanage_urls"
 }
--- models/user.go
+++ models/user.go
@@ -2,28 +2,19 @@ package models
 
 type User struct {
 	ID        uint   `gorm:"primary_key"`
-	Username  string `gorm:"type:varchar(30);unique;not null"`
-	Email     string `gorm:"type:varchar(100);not null;default:''"`
-	Name      string `gorm:"type:varchar(30);not null;default:''"`
+	Username  string `gorm:"type:varchar(64);unique;not null"`
 	Password  string `gorm:"type:varchar(64);not null;default:''"`
-	Nickname  string `gorm:"type:varchar(30);not null;default:''"`
-	Sex       string `gorm:"type:varchar(2);"`
+	Email     string `gorm:"type:varchar(128);not null;default:''"`
+	Nickname  string `gorm:"type:varchar(64);not null;default:''"`
+	Sex       string `gorm:"type:varchar(16);"`
 	Active    bool   `gorm:"not null;default:'1'"`
 	Superuser bool   `gorm:"not null;default:'1'"`
-	Role      Role   `gorm:"foreignkey:UserRoleID;association_foreignkey:RoleID"`
+	Role      Role   `gorm:"foreignkey:RoleID;association_foreignkey:ID"`
 	RoleID    uint   `json:"role_id"`
-}
-
-func (User) TableName() string {
-	return "usermanage_users"
 }
 
 type Role struct {
 	ID         uint   `gorm:"primary_key"`
-	Name       string `gorm:"type:varchar(30)"`
-	Permission []Urls `gorm:"many2many:usermanage_role_urls"`
-}
-
-func (Role) TableName() string {
-	return "usermanage_roles"
+	Name       string `gorm:"type:varchar(32)"`
+	Permission []Urls `gorm:"many2many:role_urls"`
 }
